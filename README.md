d62a32bf0f8c470a67f4a0355e7f20e3fa58d950
kosunghun317 <kosunghun317@gmail.com>
2024-10-23 01:42:49 +0900

    updated README and plot script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Synstation Research
+
+This repo contains various research topics arose while building and designing mechanism for new prediction market platform, Synstation.
--- plot_emission.py
+++ plot_emission.py
@@ -14,20 +14,40 @@ def plot_supply(M, H, time_range):
     t, Y_t = supply_over_time(M, H, time_range)
 
     plt.figure(figsize=(8, 6))
-    plt.plot(t, Y_t, label=f"M = {M}, H = {H}")
-    plt.axhline(y=M, color="r", linestyle="--", label="Maximum Emission (M)")
+    plt.plot(t, Y_t, label=f"M = {M/10**6:.2f} million, H = {H} days")
+    plt.axhline(
+        y=M, color="r", linestyle="--", label="Maximum Emission (M)"
+    )  # maximum supply
+    # halving lines
+    for i in range(1, 5):
+        if i == 1:
+            order = "st"
+        elif i == 2:
+            order = "nd"
+        elif i == 3:
+            order = "rd"
+        else:
+            order = "th"
+        plt.axvline(x=i * H, color="g", linestyle="--", label=f"{i}{order} Halving")
+
+    # grid, title and labels
+    plt.grid(True)
     plt.title("Emission over Time")
     plt.xlabel("Time (t)")
     plt.ylabel("Emission (Y(t))")
-    plt.grid(True)
     plt.legend()
+
+    # save the plot
+    plt.savefig("plots/SYN_emission.png")
+
+    # show the plot
     plt.show()
 
 
-# Example input
-# 1B Total Supply, Halving every 365 days, 4 years
-M = 1_000_000_000 * 0.5
-H = 365
-time_range = 4 * H
+if __name__ == "__main__":
+    # 1B Total Supply, Halving every 365 days, 4 years
+    M = 1_000_000_000 * 0.5
+    H = 180
+    time_range = 4 * H
 
-plot_supply(M, H, time_range)
+    plot_supply(M, H, time_range)
